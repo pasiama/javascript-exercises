aa1955a93fd173794394157594fa8578c9ce1136
Asiama Phoebe <phoebeasiama6@gmail.com>
2025-01-28 23:10:54 +0000

    variable test on cisco

---
+++ CISCO-Variable test/script.js
@@ -0,0 +1,12 @@
+const rose = 8;
+const lily = 10;
+const tulip = 2;
+const numberLilies = 50;
+const numberRoses = 70;
+const numberTulips = 120;
+const value = numberLilies * lily
+
+console.log("Rose - unit price:" + rose, "quantity:" +numberRoses, "value:" + numberRoses * rose);
+console.log("Lily - unit price:" + lily, "quantity:" +numberLilies, "value:" + numberLilies * lily);
+console.log("Tulip - unit price:" + tulip, "quantity:" +numberTulips, "value:" + numberTulips * tulip);
+console.log("total", + numberLilies + numberRoses + numberRoses)
